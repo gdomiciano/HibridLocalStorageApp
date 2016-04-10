75786bdf3d6148d6a4edfb13e66415a1d0f5e527
Francisco Kahil <codexico@gmail.com>
2016-04-10 19:55:25 -0300

    Linting

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,67 @@
+/* global $, Camera */
+
 // simple chat app
-var communicator = (function () {
+var ChatApp = {};
+
+ChatApp.accelerometer = (function () {
+    var transforms = [
+        '-webkit-transform',
+        '-moz-transform',
+        '-ms-transform',
+        'transform'
+    ];
+
+    var accelerometerOptions = {
+        'frequency': 2000
+    };
+
+    function accelerometerSuccess(data) {
+        var rotate = {};
+        var deg = 100;
+
+        // console.log('rotate!', data);
+        transforms.forEach(function (attr) {
+            var rotateX = 'rotateX(' + (data.x * deg) + 'deg)';
+            var rotateY = 'rotateY(' + (data.y * deg) + 'deg)';
+            var rotateZ = 'rotateZ(' + (data.z * deg) + 'deg)';
+
+            rotate[attr] = rotateX + rotateY + rotateZ;
+        });
+        // getUserList()
+        $('#box-phone-d1').css(rotate);
+
+        // navigator.accelerometer.clearWatch(watchID);
+    }
+
+    function accelerometerError() {
+        console.log('error');
+    }
+
+    function accelerationWatch() {
+        navigator.accelerometer.watchAcceleration(
+            accelerometerSuccess,
+            accelerometerError,
+            accelerometerOptions
+        );
+    }
+
+    function init() {
+        console.log('init accelerometer');
+        accelerationWatch();
+    }
+
+    return {
+        'init': init
+    };
+
+})(); // accelerometer
+
+ChatApp.communicator = (function () {
 
     var savedMessages = [];
     var $messageField = $('#message');
     var $messages = $('#messages');
-    var destination;
+    var destination = '';
 
     function getMessageTextHTML(msg) {
         return '<li>' + msg.value + '</li>';
@@ -22,20 +79,20 @@ var communicator = (function () {
         if (msg.type === 'text') {
             $messages.append(getMessageTextHTML(msg));
         }
-        $messages.listview("refresh");
+        $messages.listview('refresh');
     }
 
-    // deprecated
-    function renderMessages(refresh) {
-        if(savedMessages === []) {
-            $messages.html('<li>no messages</li>');
-            return;
-        }
-        $messages.html('');
-        savedMessages.forEach(function (msg) {
-            appendMessage(msg);
-        });
-    }
+    // @deprecated
+    // function renderMessages() {
+    //     if(savedMessages === []) {
+    //         $messages.html('<li>no messages</li>');
+    //         return;
+    //     }
+    //     $messages.html('');
+    //     savedMessages.forEach(function (msg) {
+    //         appendMessage(msg);
+    //     });
+    // }
 
     function addMessage(msg) {
         if (savedMessages.length === 0) {
@@ -52,32 +109,22 @@ var communicator = (function () {
 
     function onSendMessage() {
         var message = {
-            type: 'text',
-            value: $messageField.val(),
-            to:destination
-        }
+            'type': 'text',
+            'value': $messageField.val(),
+            'to': destination
+        };
+
         addMessage(message);
         // console.log('Envie Mensagem');
 
-        // socketClient.sendMessage(message.value)
-        socketClient.sendMessage(JSON.stringify(message));
-    }
-
-
-    function onGetPhoto() {
-        // console.log('tap');
-        var options = {
-            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-            destinationType: Camera.DestinationType.DATA_URL
-        }
-        navigator.camera.getPicture(onGetPhotoSuccess, onGetPhotoError, options);
+        ChatApp.socketClient.sendMessage(JSON.stringify(message));
     }
 
     function onGetPhotoSuccess(imageData) {
         var message = {
-            value: imageData,
-            type:'image64'
-        }
+            'value': imageData,
+            'type': 'image64'
+        };
         addMessage(message);
     }
 
@@ -85,165 +132,64 @@ var communicator = (function () {
         console.log('error: ', error);
     }
 
+    function onGetPhoto() {
+        // console.log('tap');
+        var options = {
+            'sourceType': Camera.PictureSourceType.PHOTOLIBRARY,
+            'destinationType': Camera.DestinationType.DATA_URL
+        };
+
+        navigator.camera.getPicture(
+            onGetPhotoSuccess, onGetPhotoError, options
+        );
+    }
+
     function onDeleteMessages() {
         window.localStorage.clear();
         savedMessages = [];
         $messages.html('<li>no messages</li>');
-        $messages.listview("refresh");
+        $messages.listview('refresh');
         // console.log('Deleta Mensagens');
     }
 
-    function initListeners(user) {
-
+    function initListeners() {
         $('#send-message').on('tap', onSendMessage);
         $('#delete-messages').on('tap', onDeleteMessages);
         $('#get-photo').on('taphold', onGetPhoto);
     }
 
     return {
-        init: initListeners,
-        getMesageFromSocket: function(msg){
-            console.log('msg',msg)
+        'init': initListeners,
+        'getMesageFromSocket': function (msg) {
+            console.log('msg', msg);
             savedMessages.push(msg);
-            window.localStorage.setItem('messages', JSON.stringify(savedMessages));
+            window.localStorage.setItem('messages',
+                JSON.stringify(savedMessages)
+            );
             $messages.append(getMessageTextHTML(msg));
-            $messages.listview("refresh");
+            $messages.listview('refresh');
         }
-    }
-
-})();
-
-// simple chat app
-var accelerometer = (function () {
-    var watchID;
-
-    var transforms = [
-        '-webkit-transform',
-        '-moz-transform',
-        '-ms-transform',
-        'transform'
-    ]
-
-    var accelerometerOptions = {
-        frequency: 2000
-    }
-
-    function accelerometerSuccess(data) {
-        var rotate = {};
-
-        // console.log('rotate!', data);
-        transforms.forEach(function (attr) {
-            var rotateX = 'rotateX(' + (data.x * 100) + 'deg)';
-            var rotateY = 'rotateY(' + (data.y * 100) + 'deg)';
-            var rotateZ = 'rotateZ(' + (data.z * 100) + 'deg)';
-
-            rotate[attr] = rotateX + rotateY + rotateZ;
-        });
-        // getUserList()
-        $('#box-phone-d1').css(rotate);
-
-        // navigator.accelerometer.clearWatch(watchID);
     };
 
-    function accelerometerError() {
-        console.log('error');
-    };
+})(); // communicator
 
-    function accelerationWatch() {
-        watchID = navigator.accelerometer.watchAcceleration(
-            accelerometerSuccess,
-            accelerometerError,
-            accelerometerOptions
-        );
-    }
-
-    function init() {
-        console.log('init accelerometer');
-        accelerationWatch();
-    }
-
-    return {
-        init: init
-    };
-
-})();
-
-var userListPage = (function(){
-    var url ='http://www.mocky.io/v2/56f0a2ef1000007f018ef257'
-
-    function onPageBeforeChange(e, data) {
-        if(data.toPage === "#page") {
-            var user = data.options.user;
-            console.log('change user', user);
-            communicator.init(user);
-            socketClient.init();
-        }
-    }
-
-    function pageChangeTap(){
-        console.log('evento change')
-        $.mobile.pageContainer.pagecontainer('change', '#page', {
-            user: $(this).attr('data-user'),
-            transition: 'flip'
-        });
-
-    }
-
-    var onGetUserListSuccess = function(data){
-        var $parent = $('#user-list')
-        data.forEach(function(user){
-
-        var newUser =  ' <li>';
-        newUser+='<a class="conversations" data-user="' + user.user + '" href="#page'+(user.user).replace('d','')+'">';
-        newUser+='<div class="square">';
-        newUser+='<img src="img/phone.png" alt="" id="box-phone-' + user.user + '">';
-        newUser+='</div>';
-        newUser+='Dupla ' + user.user;
-        newUser+='</a>';
-        newUser+='</li>';
-            $parent.append(newUser);
-        });
-        $parent.listview().listview("refresh");
-        $(document).on('tap','.conversations',pageChangeTap);
-        $(document).on('pagebeforechange', onPageBeforeChange);
-    }
-
-    var getUserList = function(){
-        $.ajax({
-            dataType: 'jsonp',
-            jsonpCallback: 'jsonCallback',
-            contentType: 'application/json',
-            url:url,
-            success: onGetUserListSuccess
-        })
-    }
-
-    return{
-        mkey:'a',
-        init:function(){
-            getUserList();
-        }
-    }
-})();
-
-
-var socketClient = (function () {
+ChatApp.socketClient = (function () {
     var connected = false;
-    var socket;
+    var socket = null;
 
     function onSocketError(error) {
         console.log(error);
     }
 
     function onSocketOpen(event) {
-        console.log('connected');
+        console.log('connected', event);
         connected = true;
     }
 
-    function onSocketMessage (event) {
+    function onSocketMessage(event) {
         var message = JSON.parse(event.data);
-        communicator.getMesageFromSocket(message)
-        console.log( message);
+        ChatApp.communicator.getMesageFromSocket(message);
+        console.log(message);
     }
 
     function onSocketClose() {
@@ -251,7 +197,7 @@ var socketClient = (function () {
     }
 
     function sendMessage(message) {
-        if(socket && connected) {
+        if (socket && connected) {
             socket.send(message);
         }
     }
@@ -263,22 +209,90 @@ var socketClient = (function () {
         socket.onopen = onSocketOpen;
         socket.onclose = onSocketClose;
     }
-    return{
-        init:init,
-        sendMessage: sendMessage
 
+    return {
+        'init': init,
+        'sendMessage': sendMessage
+    };
+
+})(); // socketClient
+
+ChatApp.userListPage = (function () {
+    var url = 'http://www.mocky.io/v2/56f0a2ef1000007f018ef257';
+    console.log('socketClient', ChatApp.socketClient);
+
+    function onPageBeforeChange(e, data) {
+        var user = data.options.user;
+        if (data.toPage === '#page') {
+            console.log('change user', user);
+            ChatApp.communicator.init(user);
+            ChatApp.socketClient.init();
+        }
     }
 
+    function pageChangeTap() {
+        console.log('evento change');
+        $.mobile.pageContainer.pagecontainer('change', '#page', {
+            'user': $(this).attr('data-user'),
+            'transition': 'flip'
+        });
+    }
 
-})();
+    function onGetUserListSuccess(data) {
+        var $parent = $('#user-list');
+        var newUserHTML = '';
 
- $(document).on("mobileinit", function () {
-    $.mobile.defaultPageTransition = "none";
-    $.mobile.defaultDialogTransition = "none";
+        data.forEach(function (user) {
+            newUserHTML += '<li>';
+            newUserHTML += '<a class="conversations" ';
+            newUserHTML += '  data-user="' + user.user + '" ';
+            newUserHTML += '  href="#page' + (user.user).replace('d', '') + '"';
+            newUserHTML += '>';
+            newUserHTML += '<div class="square">';
+            newUserHTML += '<img src="img/phone.png" alt="" ';
+            newUserHTML += '  id="box-phone-' + user.user + '"';
+            newUserHTML += '>';
+            newUserHTML += '</div>';
+            newUserHTML += 'Dupla ' + user.user;
+            newUserHTML += '</a>';
+            newUserHTML += '</li>';
+            $parent.append(newUserHTML);
+        });
 
-    userListPage.init();
-    accelerometer.init();
+        $parent.listview().listview('refresh');
+        $(document).on('tap', '.conversations', pageChangeTap);
+        $(document).on('pagebeforechange', onPageBeforeChange);
+    }
+
+    function getUserList() {
+        $.ajax({
+            'dataType': 'jsonp',
+            'jsonpCallback': 'jsonCallback',
+            'contentType': 'application/json',
+            'url': url,
+            'success': onGetUserListSuccess
+        });
+    }
+
+    return {
+        'mkey': 'a',
+        'init': function () {
+            getUserList();
+        }
+    };
+
+})(); // userListPage
+
+$(document).on('mobileinit', function () {
+    $.mobile.defaultPageTransition = 'none';
+    $.mobile.defaultDialogTransition = 'none';
+
+    ChatApp.userListPage.init();
 });
+
+document.addEventListener('deviceready', function onDeviceReady() {
+    ChatApp.accelerometer.init();
+}, false);
 
 // function onTakePhoto() {
 //     var options = {
@@ -286,5 +300,7 @@ var socketClient = (function () {
 //         destinationType : Camera.DestinationType.DATA_URL
 //     }
 
-//     navigator.camera.getPicture(oneGetPhotoSuccess, onGetPhotoError, options);
+//     navigator.camera.getPicture(
+//          oneGetPhotoSuccess, onGetPhotoError, options
+//     );
 // }
