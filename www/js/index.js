a65d64fa8b7f532281b1cff2dd48d2ebf4da2eb7
Francisco Kahil <codexico@gmail.com>
2016-04-11 15:44:51 -0300

    Move listerner to communicator

--- www/js/index.js
+++ www/js/index.js
@@ -54,11 +54,11 @@ ChatApp.accelerometer = (function () {
 })(); // accelerometer
 
 ChatApp.communicator = (function () {
-
     var savedMessages = [];
     var $messageField = $('#message');
     var $messages = $('#messages');
     var receiver = '';
+    var receiverMessages = [];
 
     function getMessageTextHTML(msg) {
         return '<li>' + msg.value + '</li>';
@@ -71,10 +71,6 @@ ChatApp.communicator = (function () {
 
     function showMessage(msg) {
         console.log('showMessage', msg);
-        if (savedMessages.length === 0) {
-            // clear "no messages" message
-            $messages.html('');
-        }
 
         if (msg.type === 'image64') {
             $messages.append(getMessageImgHTML(msg));
@@ -82,12 +78,21 @@ ChatApp.communicator = (function () {
         if (msg.type === 'text') {
             $messages.append(getMessageTextHTML(msg));
         }
-        $messages.listview('refresh');
+        $messages.listview().listview('refresh');
     }
 
     function addLocalMessage(msg) {
         console.log('addLocalMessage', msg);
         savedMessages.push(msg);
+
+        if (receiverMessages.length === 0) {
+            // clear "no messages" message
+            $messages.html('');
+        }
+        if (msg.to === receiver) {
+            receiverMessages.push(msg);
+        }
+
         window.localStorage.setItem('messages', JSON.stringify(savedMessages));
     }
 
@@ -95,7 +100,8 @@ ChatApp.communicator = (function () {
         var message = {
             'type': 'text',
             'value': $messageField.val(),
-            'to': receiver
+            'to': receiver,
+            'from': ''
         };
         $messageField.val('');
 
@@ -132,21 +138,65 @@ ChatApp.communicator = (function () {
         $messages.listview('refresh');
     }
 
+    function receiveMessage(msg) {
+        console.log('receiveMessage', msg);
+        addLocalMessage(msg);
+
+        if (msg.to === receiver) {
+            showMessage(msg);
+        }
+    }
+
+    function isReceiver(message) {
+        console.log('isReceiver', message.to);
+        return message.to === receiver;
+    }
+
+    function startChat() {
+        console.log('startChat', receiver);
+        savedMessages = JSON.parse(localStorage.getItem('messages'));
+        receiverMessages = savedMessages.filter(isReceiver);
+        console.log('receiverMessages', receiverMessages);
+
+        if (receiverMessages.length === 0) {
+            $messages.html('<li>no messages</li>');
+            $messages.listview().listview('refresh');
+        } else {
+            // clear "no messages" message
+            $messages.html('');
+        }
+
+        receiverMessages.map(showMessage);
+    }
+
+    function onPageBeforeChange(e, data) {
+        receiver = data.options.receiver;
+
+        console.log('onPageBeforeChange');
+        console.log('data.toPage', data.toPage);
+
+        if (data.toPage === '#page') {
+            // navigation is about to commence
+            console.log('talk to', receiver);
+            startChat();
+        } else {
+            // the destination page has been loaded and navigation will continue
+            $('#receiver').html(receiver);
+        }
+    }
+
     function initCommunicator() {
         $('#send-message').on('tap', sendTextMessage);
         $('#delete-messages').on('tap', deleteMessages);
         $('#get-photo').on('taphold', getPhotoFromLibrary);
-    }
 
-    function receiveMessage(msg) {
-        console.log('receiveMessage', msg);
-        showMessage(msg);
-        addLocalMessage(msg);
+        $(document).on('pagebeforechange', onPageBeforeChange);
     }
 
     return {
         'init': initCommunicator,
-        'receiveMessage': receiveMessage
+        'receiveMessage': receiveMessage,
+        'startChat': startChat
     };
 
 })(); // communicator
@@ -181,9 +231,11 @@ ChatApp.socketClient = (function () {
     }
 
     function initSocketClient() {
+        console.log('initSocketClient');
         if (socket && isConnected) {
             return;
         }
+        console.log('initSocketClient2');
         socket = new WebSocket('ws://127.0.0.1:1337');
         socket.onmessage = onSocketMessage;
         socket.onerror = onSocketError;
@@ -200,27 +252,6 @@ ChatApp.socketClient = (function () {
 
 ChatApp.usersListPage = (function () {
     var url = 'http://www.mocky.io/v2/56f0a2ef1000007f018ef257';
-
-    function initChat(receiver) {
-        ChatApp.communicator.init(receiver);
-        ChatApp.socketClient.init();
-    }
-
-    function onPageBeforeChange(e, data) {
-        var receiver = data.options.receiver;
-
-        console.log('onPageBeforeChange');
-        console.log('data.toPage', data.toPage);
-
-        if (data.toPage === '#page') {
-            // navigation is about to commence
-            console.log('talk to', receiver);
-            initChat(receiver);
-        } else {
-            // the destination page has been loaded and navigation will continue
-            $('#receiver').html(receiver);
-        }
-    }
 
     function goToChatPage() {
         console.log('goToChatPage');
@@ -266,7 +297,6 @@ ChatApp.usersListPage = (function () {
 
     function initListeners() {
         $(document).on('tap', '.conversations', goToChatPage);
-        $(document).on('pagebeforechange', onPageBeforeChange);
     }
 
     function initUsersListPage() {
@@ -286,6 +316,8 @@ $(document).on('mobileinit', function () {
     $.mobile.defaultDialogTransition = 'none';
 
     ChatApp.usersListPage.init();
+    ChatApp.communicator.init();
+    ChatApp.socketClient.init();
 });
 
 document.addEventListener('deviceready', function onDeviceReady() {
